3f74c7a17e89cff38df947601b867984532d726c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-19 11:09:18 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -6,7 +6,10 @@ cd macOS-Simple-KVM
 sudo python3  -m pip install click
 ./jumpstart.sh --mojave
 wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
-HEADLESS=0 MEM=7G CPUS=8 SYSTEM_DISK=MyDisk.qcow2 ./headless.sh
+cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
+availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
+availableRAM=$(echo $availableRAMcommand | bash)
+HEADLESS=0 MEM=$availableRAMG CPUS=$cpus SYSTEM_DISK=MyDisk.qcow2 ./headless.sh
 echo "All Done !!! Your Mac OS VM now Ready to use !!!"
 echo "User: Mojave-User"
 echo "Pass: 123456"
