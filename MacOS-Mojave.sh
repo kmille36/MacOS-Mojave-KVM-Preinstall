16869c1c53ae0f847e595262cdc1bb46f12de0e3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-19 11:05:10 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -7,3 +7,6 @@ sudo python3  -m pip install click
 ./jumpstart.sh --mojave
 wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
 HEADLESS=0 MEM=7G CPUS=8 SYSTEM_DISK=MyDisk.qcow2 ./headless.sh
+echo "All Done !!! Your Mac OS VM now Ready to use !!!"
+echo "User: Mojave-User"
+echo "Pass: 123456"
