e314c62793a00b62312b7288e51c03d244d6bb2f
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-19 11:00:57 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -1,9 +1,9 @@
-echo Building...
-apt update
+echo Installing Software...
+sudo apt install qemu-system qemu-utils python3 python3-pip -y 
+sudo apt update
 git clone https://github.com/foxlet/macOS-Simple-KVM.git
 cd macOS-Simple-KVM
-wget -q --show-progress --no-check-certificate --progress=dot:giga -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
 sudo python3  -m pip install click
 ./jumpstart.sh --mojave
-sudo apt install qemu-system qemu-utils python3 python3-pip -y 
+wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
 HEADLESS=0 MEM=7G CPUS=8 SYSTEM_DISK=MyDisk.qcow2 ./headless.sh
